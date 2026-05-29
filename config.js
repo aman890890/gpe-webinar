4f136fae539fca1b23c6ec3164a05401792ebf03
aman890890 <amanubercar@gmail.com>
2026-05-29 09:49:15 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
  * Edit GAS_EXEC_URL dengan URL deploy Apps Script anda (/exec)
  */
 const GPE_WEBINAR_CONFIG = {
-  GAS_EXEC_URL: "https://script.google.com/macros/s/AKfycbwp1NOHncz5wFkwKyJlR4B6D7fLoHWAu2nO9racYLOHuwopsxubgD7FlWL4is_lFusCYg/exec",
+  GAS_EXEC_URL: "https://script.google.com/macros/s/AKfycbyKRt1knxIkxF-of-QnJXtdzP_jTAPewFB96Sy_x4u_eBE5Ubn15j5yz-UJiuN7gguJ/exec",
   MAX_FILE_MB: 10
 };
